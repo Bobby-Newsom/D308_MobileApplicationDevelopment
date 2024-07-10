b233dd7afaaeb83b08cd67f71b016df585db894a
Bobby Newsom <bobby.newsom@gmail.com>
2024-07-10 11:58:18 -0500

    Initial backup commit

---
+++ app/src/main/java/com/example/bobbynewsom_001265608/UI/VacationList.java
@@ -0,0 +1,29 @@
+package com.example.bobbynewsom_001265608.UI;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.bobbynewsom_001265608.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class VacationList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_vacation_list);
+        FloatingActionButton fab=findViewById(R.id.floatingActionButton);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+    }
+}
