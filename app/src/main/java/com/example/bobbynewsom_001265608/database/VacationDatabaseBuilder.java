d6909967546af4c41d69268c04271d3e8b057b50
Bobby Newsom <bobby.newsom@gmail.com>
2024-09-18 12:42:07 -0500

    finished VacationDatabaseBuilder

--- app/src/main/java/com/example/bobbynewsom_001265608/database/VacationDatabaseBuilder.java
+++ app/src/main/java/com/example/bobbynewsom_001265608/database/VacationDatabaseBuilder.java
@@ -1,6 +1,9 @@
 package com.example.bobbynewsom_001265608.database;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 import com.example.bobbynewsom_001265608.dao.ExcursionDAO;
@@ -12,8 +15,25 @@ import com.example.bobbynewsom_001265608.entities.Vacation;
 public abstract class VacationDatabaseBuilder extends RoomDatabase {
 
     private static VacationDatabaseBuilder instance;
+
     public abstract VacationDAO vacationDAO();
+
     public abstract ExcursionDAO excursionDAO();
 
+    private static volatile VacationDatabaseBuilder INSTANCE;
 
+    static VacationDatabaseBuilder getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (VacationDatabaseBuilder.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                                    VacationDatabaseBuilder.class, "vacation_database.db")
+                                    .fallbackToDestructiveMigration()
+                                    .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
 }
+
